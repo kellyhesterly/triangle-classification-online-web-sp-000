237490281d79819e76393f8305edd9088e848334
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-18 15:55:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,53 +1,39 @@
 require 'pry'
-class Triangle
-  # write code here
-  attr_accessor :side_one, :side_two, :side_three, :sides
 
-  @sides = []
+class Triangle
+    attr_accessor :side_one, :side_two, :side_three, :equilateral, :isosceles, :scalene
 
   def initialize(side_one, side_two, side_three)
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
-    @sides = [side_one, side_two, side_three]
   end
 
   def kind
-    if (negative? == true) || (valid? == false)
-        raise TriangleError
-      elsif(equilateral?)
-        :equilateral
-      elsif(scalene?)
-        :scalene
-      elsif(isosceles?)
-        :isosceles
-      end
-  end
-
-  def negative?
-    @sides.each do |length|
-      if(length <= 0)
-        return true
+    # binding.pry
+    if self == equilateral
+      :equilateral
+    elsif self == isosceles
+      :isosceles
+    else self == scalene
+      :scalene
     end
   end
+
+  def equilateral
+    (side_one == side_two) && (side_two == side_three) && (side_one == side_three)
+  end
+
+  def isosceles
+    (side_one == side_two) || (side_one == side_three) || (side_two == side_three)
+  end
+
+  def scalene
+    (side_one != side_two) && (side_two != side_three) && (side_one != side_three)
+  end
+
+class TriangleError < StandardError
+
 end
 
-  def valid?
-    (@side_one + @side_two > @side_three) && (@side_one + @side_three > @side_two) && (@side_two + @side_three > @side_one)
-  end
-
-  def equilateral?
-    (@side_one == @side_two) && (@side_one == @side_three)
-  end
-
-  def isosceles?
-    (@side_one == @side_two) || (@side_one == @side_three) || (@side_two == @side_three)
-  end
-
-  def scalene?
-    (@side_one != @side_two) && (@side_one != @side_three) && (@side_two != @side_three)
-  end
-  class TriangleError < StandardError
-
-  end
 end
